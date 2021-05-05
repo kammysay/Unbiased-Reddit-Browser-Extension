55df7a50134ea59b060b8397fc762d975144569b
Samuel Kuchek <samuelkuchek@gmail.com>
2021-05-05 15:36:29 -0400

    Fixed frontpage issue

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 // Initial function calls
 hideVotes();
-hideAwards();
 
 // When mouse scrolled, call functions to keep up with newly loaded posts/comments
-window.addEventListener('scroll', function(e) {
+window.addEventListener('wheel', function() {
     hideVotes();
-    hideAwards();
+});
+
+// When mouse clicked, call functions to keep up with newly loaded posts/comments
+window.addEventListener('click', function() {
+    hideVotes();
 });
 
 // Hides the number of votes on a post/comment
@@ -16,24 +19,13 @@ function hideVotes(){
         postVotes[i].remove();
     }
 
+    // There is only ever one banner vote
+    bannerVote = document.getElementsByClassName("_1rZYMD_4xY3gRcSS3p8ODO _25IkBM0rRUqWX5ZojEMAFQ");
+    bannerVote[0].remove();
+
     // Remove div for vote count on comments
     commentVotes = document.getElementsByClassName("_1rZYMD_4xY3gRcSS3p8ODO _25IkBM0rRUqWX5ZojEMAFQ _3ChHiOyYyUkpZ_Nm3ZyM2M");
     for( i=0; i<commentVotes.length; i++ ){
         commentVotes[i].remove();
-    }
-}
-
-// Hide any awards and box-shadows on comments
-function hideAwards(){
-    // _3XoW0oYd5806XiOr24gGdb is the award panel on reddit
-    banners = document.getElementsByClassName("_3XoW0oYd5806XiOr24gGdb");
-    for( i=0; i<banners.length; i++){
-        banners[i].remove(); // remove award panel
-    }
-
-    // Remove any box shadows surrounding comments
-    comments = document.querySelectorAll('[role="presentation"]');
-    for( i=0; i<comments.length; i++ ){
-        comments[i].remove();
     }
 }
